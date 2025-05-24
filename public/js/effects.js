fb9d2ec15f4b5d362a6d9c198d37f7e0f08d1cd7
GiorgosLillis <giorgoslillis6@gmail.com>
2025-05-24 19:35:16 +0300

    Final

--- public/js/effects.js
+++ public/js/effects.js
@@ -9,15 +9,25 @@ AOS.init({
     
 const savedTheme = localStorage.getItem('site-theme') || 'default';
  setTheme(savedTheme);
-
-
-
 function setTheme(theme) {
   document.body.classList.remove('theme-default', 'theme-light', 'theme-dark');
   document.body.classList.add(`theme-${theme}`);
   localStorage.setItem('site-theme', theme);
 }
 
+const current_date = document.getElementById('current-date');
+const current_time = document.getElementById('current-time');
 
+let date = new Date();
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+current_date.innerHTML = date.toLocaleDateString('en-GB', options);
+
+let time = date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+current_time.innerHTML = time;
+setInterval(() => {
+    let date = new Date();
+    let time = date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    current_time.innerHTML = time;
+}, 1000);
 
 
